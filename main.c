e5e342c00a86b5f875053e80952f8fbe43833dac
Juan1ta-C <94313908+Juan1ta-C@users.noreply.github.com>
2025-03-20 19:39:54 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
+#include "file.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "string.h"
+
+
+void *createDirectory(Directory *parent, char *name);
+void *createFile(Directory *parent, char *name, int size);
+void listDirectory(Directory *dir);
+
 int main(int argc, char * * argv) {
 
+    Directory *root = createDirectory(NULL, "root");
+    int choice;
+    char name[MAXSIZE];
+    int size;
 
     while (1) {
-        // Display menu
         printf("\nMenu:\n");
         printf("1. Create New Directory\n");
         printf("2. Create New File\n");
@@ -14,33 +27,36 @@ int main(int argc, char * * argv) {
 
         switch (choice) {
             case 1: 
-                
                 printf("Enter Directory Name: ");
-                scanf(" %[^\n]", name);  
-          
-                createDirectory();
-				
+                scanf(" %s", name);   
+                createDirectory(root, name);
                 break;
 
-            case 2: // Display pets
-                displayAllAlienPets(pets, count);
+            case 2:
+                printf("Enter File Name: ");
+                scanf(" %s", name);
+                printf("Enter File Size: ");
+                scanf("%d", &size);
+                createFile(root, name, size);
                 break;
 
-            case 3: // Exit
-                
-                freeFile();
+            case 3:
+                listDirectory(root);
+                break;
+
+            case 4:
+                printf("Invalid choice. Please try again.\n");
+                break;
+
+            case 5:
+                printf("Invalid choice. Please try again.\n");
                 return 0;
 
             default:
-                
+                printf("Invalid choice. Please try again.\n");
                 break;
         }
     }
 
-    return 0; 
-
-
-
-
-
+    return 0;
 }
