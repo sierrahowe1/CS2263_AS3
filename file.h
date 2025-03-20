d41244b6951e18bf50f66192adafe6bb24ba8ffd
Juan1ta-C <94313908+Juan1ta-C@users.noreply.github.com>
2025-03-20 19:45:19 -0300

    Update file.h

--- file.h
+++ file.h
@@ -1,21 +1,25 @@
 #ifndef FILE_H
 #define FILE_H
+
 #define MAXSIZE 20
 
-typedef struct { 
-   char *name;
-   int childCount;
-   int sibCount;
-   int fileCount;
-   Directory *parent;
-   Dirctory *child[MAXSIZE];
-   Directory *siblings[MAXSIZE];
-   File *file[MAXSIZE];
-}Directory;
+typedef struct Directory Directory;
 
 typedef struct {
    char *name;
    int size;
    Directory *parent;
-}File;
+} File;
+
+typedef struct Directory { 
+   char *name;
+   int childCount;
+   int sibCount;
+   int fileCount;
+   Directory *parent;
+   Directory *child[MAXSIZE];
+   Directory *siblings[MAXSIZE];
+   File *file[MAXSIZE];
+} Directory;
+
 #endif
